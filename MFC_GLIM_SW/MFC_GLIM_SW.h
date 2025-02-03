a5d7461f25ea2fa28f581d42826ad6639c03b092
sysyuseok <sonyous0513@naver.com>
2025-02-03 19:50:47 +0900

    Add project files.

---
+++ MFC_GLIM_SW/MFC_GLIM_SW.h
@@ -0,0 +1,32 @@
+
+// MFC_GLIM_SW.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CMFCGLIMSWApp:
+// See MFC_GLIM_SW.cpp for the implementation of this class
+//
+
+class CMFCGLIMSWApp : public CWinApp
+{
+public:
+	CMFCGLIMSWApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCGLIMSWApp theApp;
